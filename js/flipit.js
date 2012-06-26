970f7f843c70e062cd1f3d45945e9f5a49c62fdb
Ethan Heilman <ethan@geographicslab.org>
2012-06-26 19:49:57 -0400

    Basic site layout and structure

---
+++ js/flipit.js
@@ -0,0 +1,14 @@
+
+
+function FlipItGame( board_id, start_btn_id, flip_btn_id, score_id ){
+
+  var board = $("#game_board")[0];
+
+  //setup board
+  var c = board.getContext("2d");
+  c.fillStyle = "#111111";
+  c.fillRect(0,0,800,200);
+
+  //setup buttons
+}
+
